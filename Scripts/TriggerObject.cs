34b564ac2a7f56417b2b58940c4e9eb497f80ca6
Lee-Roy <ltd1999@hotmail.co.uk>
2024-09-14 16:44:18 +0100

    Added a comment for clarity.
    Functions are already self describing.

--- Scripts/TriggerObject.cs
+++ Scripts/TriggerObject.cs
@@ -6,6 +6,7 @@ public partial class TriggerObject : Area3D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		// Connect the signals for the Area3D
 		this.BodyEntered += OnBodyEntered;
 		this.BodyExited += OnBodyExited;
 	}
