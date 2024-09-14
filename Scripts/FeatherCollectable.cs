f8582fbcf0fab788a16252f294925d2f29eab72f
Lee-Roy <ltd1999@hotmail.co.uk>
2024-09-14 17:55:22 +0100

    Tested and changed To correct check.
    Now checks for a CharacterBody3D, this needs to be changed to the player class when it is made.

--- Scripts/FeatherCollectable.cs
+++ Scripts/FeatherCollectable.cs
@@ -5,7 +5,7 @@ public partial class FeatherCollectable : TriggerObject
 {
 	protected override void OnBodyEntered(Node body)
 	{
-		if (body is CharacterBody2D)
+		if (body is CharacterBody3D)
 		{
 			CollectableManager.Instance.CollectFeather();
 			this.QueueFree();
