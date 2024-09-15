e85d0269cad3acb9a42cf88683c45782a474413a
Stondonk <jastical66@gmail.com>
2024-09-15 18:59:16 +0100

    g

--- Scripts/UI/Settings/SettingTabBarSelect.cs
+++ Scripts/UI/Settings/SettingTabBarSelect.cs
@@ -3,7 +3,7 @@ using System;
 
 public partial class SettingTabBarSelect : Control
 {
-	[Export] public OptionButton[] TabButtons;
+	[Export] public MenuButton[] TabButtons;
 	[Export] public float PaddingH = 2.0f;
 	[Export] public float PaddingV = 2.0f;
 
@@ -12,11 +12,13 @@ public partial class SettingTabBarSelect : Control
 	public override void _Ready()
 	{
 		ICloseAllMenu();
+		int LastStart = 0;
 		for(int i=0; i < TabButtons.Length; i++){
 			TabButtons[i].Size = new Vector2((Size.X / TabButtons.Length)  - (PaddingH), TabButtons[i].Size.Y - (PaddingV));
 			TabButtons[i].Position = new Vector2((Size.X / TabButtons.Length)*i + (PaddingH/2), + (PaddingV/2));
-			if (i < TabMenus.Length)
-				TabButtons[i].Pressed += () => OpenMenu(i);
+			LastStart += TabButtons[i].ItemCount-1;
+			//if (i < TabMenus.Length)
+				//TabButtons[i].ItemSelected += OpenMenu();
 		}
 	}
 
@@ -31,11 +33,11 @@ public partial class SettingTabBarSelect : Control
 			Menu.ProcessMode = ProcessModeEnum.Disabled;
 		}
 	}
-	public void OpenMenu(int i){
-		GD.Print(TabMenus[i].Name);
+	private void OpenMenu(Godot.OptionButton.ItemSelectedEventHandler i){
+		GD.Print("Select");
 		ICloseAllMenu();
-		Control Menu = TabMenus[i];
-		Menu.Visible = true;
-		Menu.ProcessMode = ProcessModeEnum.Inherit;
+		//Control Menu = TabMenus[i];
+		//Menu.Visible = true;
+		//Menu.ProcessMode = ProcessModeEnum.Inherit;
 	}
 }
