06272c6dbadde406445ae299a4a782f64a7c8556
David Mulgrew <bigmonmulgrew@users.noreply.github.com>
2024-09-16 15:41:42 +0100

    Update Teleporter.cs

--- Scripts/Teleporter.cs
+++ Scripts/Teleporter.cs
@@ -6,20 +6,6 @@ public partial class Teleporter : Node3D
 {
 	// Configuration
 	[Export] Node3D teleportTarget;
-	
-	// Runtime variables
-	private Vector3 teleportTargetPosition;
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		
-	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-		
-	}
 
 	public void _on_area_3d_body_entered(Node other)
 	{
