a4af041bf609177de21b4ed6449e80825d53303b
David Mulgrew <bigmonmulgrew@users.noreply.github.com>
2024-09-17 19:00:49 +0100

    added level manager reload mothod

--- Scenes/_levels/level_manager.gd
+++ Scenes/_levels/level_manager.gd
@@ -9,15 +9,16 @@ const SPLASH_SCREEN = preload("res://Scenes/_menus/splash_screen.tscn")
 const WIN_SCREEN = preload("res://Scenes/_menus/win_screen.tscn")
 
 # Runtime Variables
-var previous_scene = null
+var current_scene = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	if previous_scene == null:
-		previous_scene = SPLASH_SCREEN
+	if current_scene == null:
+		current_scene = SPLASH_SCREEN
 	
-func load_previous_scene():
-	_change_scene(previous_scene)
+func load_current_scene():
+	_change_scene(current_scene)
+	
 func show_credits():
 	_change_scene(CREDITS_MENU)
 
@@ -36,17 +37,20 @@ func show_win_screen():
 func load_scene_by_path(scene: String):
 	_change_scene(scene)
 	
+func reload_scene():
+	_change_scene(current_scene)
+	
 func _change_scene(scene_path):
 	# Load the new scene
 	var scene_resource = load(scene_path)
 	var new_scene = scene_resource.instantiate()
 	
 	# Remove the existing scene if it exists
-	if previous_scene:
-		previous_scene.queue_free()
+	if current_scene:
+		current_scene.queue_free()
 		
 	# Add the new scene to the root
 	get_tree().root.add_child(new_scene)
 	
 	# Update the reference to the current scene
-	previous_scene = new_scene
+	current_scene = new_scene
