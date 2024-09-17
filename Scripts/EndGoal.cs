bdca658bb3f2588c09f3a99d223e6e1e3abc418d
David Mulgrew <bigmonmulgrew@users.noreply.github.com>
2024-09-17 20:45:04 +0100

    added an end goal

--- Scripts/EndGoal.cs
+++ Scripts/EndGoal.cs
@@ -1,13 +1,41 @@
 using Godot;
-using System;
 
+[Tool]
 public partial class EndGoal : Node3D
 {
-	[Export] PackedScene next_level;
-	public void _on_area_3d_body_entered(Node other)
+	private PackedScene _nextLevel;
+	private string _nextLevelPath = "";
+
+	[Export]
+	public PackedScene NextLevel
+	{
+		get => _nextLevel;
+		set
+		{
+			_nextLevel = value;
+			_nextLevelPath = _nextLevel != null ? _nextLevel.ResourcePath : "";
+		}
+	}
+	
+	public override void _Ready()
+	{
+		var area = GetNode<Area3D>("Area3D"); // Relative path to the child node
+		area.BodyEntered += _on_Area3D_body_entered;
+	}
+	
+	public void _on_Area3D_body_entered(Node3D other)
 	{
 		if (!other.IsInGroup("Player")) return;
 		
-		GetNode("/root/LevelManager").Call("load_scene_by_path",next_level);
+		var levelManager = GetNode("/root/LevelManager");
+		if (levelManager != null)
+		{
+			if (!string.IsNullOrEmpty(_nextLevelPath)) levelManager.Call("load_scene_by_path", _nextLevelPath);
+			else 									  levelManager.Call("reload_scene");
+		}
+		else
+		{
+			GD.PrintErr("LevelManager not found in the scene tree.");
+		}
 	}
 }
