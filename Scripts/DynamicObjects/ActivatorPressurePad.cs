ca2cabe3fd9345902bb083f1173d2013046efa72
David Mulgrew <bigmonmulgrew@users.noreply.github.com>
2024-09-19 20:45:38 +0100

    added pressure pad plus many examples to debug scene

--- Scripts/DynamicObjects/ActivatorPressurePad.cs
+++ Scripts/DynamicObjects/ActivatorPressurePad.cs
@@ -1,18 +1,18 @@
 using Godot;
 using System;
-using System.Collections.Generic;
 
 namespace GodotTemplate.Scripts;
 
 public partial class ActivatorPressurePad : Activator
 {
+     
+    
+    // Configuration
     [Export] private bool triggerOnExit = false;
-    [Export] private float depressionDistance = 0.05f;
+    [Export] float depressionDistance = 0.05f;
 
+    // Cached references
     private MeshInstance3D mesh;
-    private Queue<Action> actionsQueue = new Queue<Action>();
-    private bool isProcessing = false;
-
     public override void _Ready()
     {
         base._Ready();
@@ -22,40 +22,17 @@ public partial class ActivatorPressurePad : Activator
     public void _on_area_3d_body_entered(Node other)
     {
         if (!other.IsInGroup("Player")) return;
-
+        
         mesh.Position -= new Vector3(0, depressionDistance, 0);
-        QueueUse();
+        Use();
     }
 
     public void _on_area_3d_body_exited(Node other)
     {
         if (!other.IsInGroup("Player")) return;
-
+        
         mesh.Position += new Vector3(0, depressionDistance, 0);
-        if (triggerOnExit) QueueUse();
-    }
-
-    private void QueueUse()
-    {
-        actionsQueue.Enqueue(Use);
-        if (!isProcessing)
-        {
-            ProcessQueue();
-        }
-    }
-
-    private void ProcessQueue()
-    {
-        if (actionsQueue.Count == 0)
-        {
-            isProcessing = false;
-            return;
-        }
-
-        isProcessing = true;
-        Action action = actionsQueue.Dequeue();
-        action.Invoke();
-        // Consider using a delay or checking conditions before processing the next item.
-        CallDeferred(nameof(ProcessQueue));
+        if (!triggerOnExit) return;
+        Use();
     }
 }
