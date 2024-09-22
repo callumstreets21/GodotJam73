dc0d51321c31257a5967b048a5416302361ecca2
MutantGinga <haydenwilliamhefford@yahoo.com>
2024-09-22 18:06:44 +0100

    Added Health Component to player
    Also added call to levelManager in player die method

--- Scripts/HealthComponent.cs
+++ Scripts/HealthComponent.cs
@@ -5,69 +5,61 @@ namespace GodotTemplate.Scripts;
 
 public partial class HealthComponent : Node3D
 {
-    [Export]
-    private float health;
-    [Export]
-    private bool isInvincible;
-    [Export] 
-    private float maxHealth;
-    private bool isDead;
-    private Node3D parent_class;
-    
-    public float Health
-    {
-        get { return health; }
-        set { health = value;
-            if (health <= 0)
-            {
-                Die();
-            }
-        }
-    }
-    
-    public override void _Ready()
-    {
-        base._Ready();
-        health = maxHealth;
-        parent_class = GetParent<Node3D>();
-    }
-    
-    public void TakeDamage(float damage)
-    {
-        if (!isInvincible)
-        {
-            health -= damage;
-            if (health <= 0)
-            {
-                Die();
-            }
-        }
-    }
-    
-    public void Heal(float healAmount)
-    {
-        health += healAmount;
-        if (health > maxHealth)
-        {
-            health = maxHealth;
-        }
-    }
-    
-    public virtual void Die()
-    {
-        // empty for now
-        // override in child classes
-    }
-    
-    public virtual void Respawn()
-    {
-        // empty for now
-        // override in child classes
-    }
-    
-    public void SetInvincible(bool invincible)
-    {
-        isInvincible = invincible;
-    }
+	[Export]
+	private float health;
+	[Export]
+	private bool isInvincible;
+	[Export] 
+	private float maxHealth;
+	private bool isDead;
+	private Node3D parent_class;
+	
+	public float Health
+	{
+		get { return health; }
+		set { health = value;
+			if (health <= 0)
+			{
+				Die();
+			}
+		}
+	}
+	
+	public override void _Ready()
+	{
+		base._Ready();
+		health = maxHealth;
+		parent_class = GetParent<Node3D>();
+	}
+	
+	public void TakeDamage(float damage)
+	{
+		if (!isInvincible)
+		{
+			health -= damage;
+			if (health <= 0)
+			{
+				Die();
+			}
+		}
+	}
+	
+	public void Heal(float healAmount)
+	{
+		health += healAmount;
+		if (health > maxHealth)
+		{
+			health = maxHealth;
+		}
+	}
+	
+	public virtual void Die()
+	{
+		GetNode("root/LevelManager").Call("reload_scene");
+	}
+	
+	public void SetInvincible(bool invincible)
+	{
+		isInvincible = invincible;
+	}
 }
-
