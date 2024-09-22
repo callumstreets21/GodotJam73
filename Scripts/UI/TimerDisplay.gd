1f0efeb5270b85134bc5c6dd20c9c18b0632d800
Lee-Roy <ltd1999@hotmail.co.uk>
2024-09-22 19:56:13 +0100

    Fixed everything know to man

--- Scripts/UI/TimerDisplay.gd
+++ Scripts/UI/TimerDisplay.gd
@@ -3,5 +3,5 @@ extends Label
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	LevelManager.stop_timer()
 	text = str(LevelManager.timer) + " seconds"
+	Input.mouse_mode = Input.MouseMode.MOUSE_MODE_VISIBLE
--- Scripts/ending_cutscene.gd
+++ Scripts/ending_cutscene.gd
@@ -1,4 +1,7 @@
 extends VideoStreamPlayer
 
+func _ready() -> void:
+	LevelManager.stop_timer()
+
 func _on_finished() -> void:
 	LevelManager.show_win_screen()
